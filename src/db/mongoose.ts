52f43005ffa4fc023efe085f5c65e2779bd4f9cf
wanglin <wanglin46@lenovo.com>
2020-10-22 10:41:35 +0800

    fix:修复bug

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -3,18 +3,9 @@ import mongoose from 'mongoose'
 import chalk from 'chalk'
 import config from '../config'
 
-// const NODE_ENV = {
-//   development: 'dev',
-//   production: 'prod',
-//   test: 'test'
-// }
+let env = process.env.NODE_ENV || 'prod'
+let { username, password, db_name } = config[env]
 
-// let env = process.env.NODE_ENV || 'production'
-console.log(process.env.NODE_ENV, '===>')
-let env = 'production'
-console.log(chalk.greenBright(env))
-// let { username, password, db_name } = config[NODE_ENV[env]]
-let { username, password, db_name } = config['dev']
 let db = `mongodb://${username}:${password}@140.143.2.162:27017/${db_name}?authSource=admin`
 
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
